7a0a38216dd55ed6bd15234906a73a2a1b469884
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2021-12-17 14:43:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const fs = require('fs')
+const os = require('os');
+const pdf = require('pdf-parse');
+const ipc = require('electron').ipcMain;
+
+
+const {app, BrowserWindow,Menu, dialog, session,remote} = electron;
+let mainWindow;
+app.on('ready', ()=>{
+    //create new window
+    mainWindow = new BrowserWindow({ 
+        show: true,
+        width:800,
+        maxWidth:800,
+        maxHeight:800,
+        height:800,
+        webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true
+    }});
+
+    //Load html into window
+    mainWindow.loadURL(url.format({
+        pathname:path.join(__dirname, "view/mainWindow.html"),
+        protocol:'file:',
+        slashes:true,
+       
+    }))
+    
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    //insert menu
+    Menu.setApplicationMenu(mainMenu)
+    
+})
+
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q':
+                'Ctrl+Q',
+                click(){
+                    app.quit()
+                }
+
+        }
+        ]
+    }
+];
+mainMenuTemplate.push({label:"Developer Tools",
+    submenu:[
+        {
+            label:'Toogle DevTools',
+            accelerator: process.platform == 'darwin' ? 'Command+I' :
+            'Ctrl+I',
+            click(item,focusedWindow){
+                focusedWindow.toggleDevTools()
+            }
+        },
+        {
+            role:"reload"
+        }
+    ]
+
+
+})
+ipc.on('close-me', (evt, arg) => {
+    app.quit()
+  })
+
+ipc.on('open-folder-dialog', function (event) {
+    console.log('xd')
+    if(os.platform() === 'linux' || os.platform() === 'win32'){
+        dialog.showOpenDialog({
+            properties: ['openDirectory']
+         }).then((e)=>{
+            event.sender.send('selected-file', e.filePaths)
+
+         })
+   } else {
+     dialog.showOpenDialog({
+    properties: ['openDirectory']
+     }).then((e)=>{
+        event.sender.send('selected-file', e.filePaths)
+
+     })
+  
+   }});
+
+   ipc.on('read_pdf_file',(event,dataBuffer)=>{
+    pdf(dataBuffer).then(function(data) {
+        event.sender.send('pdf_file',data)
+    })
+    .catch(function(error){
+        // handle exceptions
+        event.sender.send('pdf_file','error')
+    })
+})
---
+++ view/js.js
@@ -0,0 +1,119 @@
+const fs = require('fs')
+const os = require('os')
+const ipc = require('electron').ipcRenderer;
+const csv = require('csvtojson');
+const converter = require('json-2-csv');
+const homeDir = require('os').homedir()
+const desktopDir = `${homeDir}/Desktop/`;
+
+let folder = undefined;
+const choose_folder = document.querySelector('#choose_folder');//przycisk do wyboru folderu
+
+//wybieram folder
+choose_folder.addEventListener('click',()=>{
+    ipc.send('open-folder-dialog')
+})
+//zwraca mi folder
+ipc.on('selected-file', function (event, path) {
+    folder = path[0]
+});
+let array_of_buffers = []
+let text_from_pdfs = []
+
+//teraz przycisk start
+
+ipc.on('pdf_file', async function (event, data) {
+    if(typeof data === 'string' && data == 'error'){
+        alert('Nie udało się przetworzyć jednego z plików. Jest on uszkodzony bądź niepoprawny. Program zakończył pracę!')
+        ipc.send('close-me')
+    }
+    text_from_pdfs.push(data.text)
+})
+
+const clear_and_write = (d)=>{
+    let data = d
+    let tmp_keys
+    for(let i=0;i<data.length;i++){
+        tmp_keys = Object.keys(data[0])
+        for(let j=0;j<tmp_keys.length;j++){
+            if(data[i][tmp_keys[j]].includes('zł'))
+            data[i][tmp_keys[j]] = data[i][tmp_keys[j]].replace('zł','')
+            data[i][tmp_keys[j]] = data[i][tmp_keys[j]].trim()
+        }
+    }
+
+    converter.json2csv(data, (err, csv) => {
+        if (err) {
+               alert("Błąd krytyczny programu, spróbuj ponownie.")
+             ipc.send('close-me')
+        }
+        fs.writeFileSync(`${desktopDir}Pliczek_vat_ceny.csv`, csv);
+        alert("Plik został zapisany na pulpicie pod nazwa Pliczek_vat_ceny.csv.")
+        ipc.send('close-me')
+    });
+}
+
+
+const conver = (el)=>{
+    return new Promise((res,rej)=>{
+        tmp=[]
+        csv({ignoreEmpty: false,delimiter:";",noheader:true})
+        .fromString(el)
+        .subscribe((jsonObj,index)=>{
+            tmp.push(jsonObj)
+        })
+        .on('done',async()=>{
+            res(tmp)
+        })
+    })
+}
+
+const conver_to_json = ()=>{
+    let array_of_ready_csv = []
+    text_from_pdfs.forEach(async(element,index,array)=>{
+        let tmp;
+        pdf_splitet = element.split("Dokument wygenerowany przez IdoSell")
+        for(let i =0;i<pdf_splitet.length;i++){
+            tmp = await conver(pdf_splitet[i])
+            array_of_ready_csv = array_of_ready_csv.concat(tmp)
+        }
+        if(index +1 == array.length)
+        clear_and_write(array_of_ready_csv)
+       
+    })
+
+}
+
+
+
+const interval_check = (length)=>{
+    const x = setInterval(() => {
+        if(length == text_from_pdfs.length){
+            clearInterval(x)
+            conver_to_json()
+        }
+    }, 3000);
+}
+const read_files = ()=>{
+    interval_check(array_of_buffers.length)
+    array_of_buffers.forEach(e=>{
+        ipc.send('read_pdf_file',e)
+     
+    })
+}
+const start_button = document.querySelector("#start_button")
+start_button.addEventListener('click',()=>{
+    if(folder != undefined){
+        choose_folder.disabled= true
+        start_button.disabled = true
+        fs.readdir(`${folder}`,(err,files)=>{
+            alert(`Program wykrył ${files.length}szt. plików, sprawdź czy ilość jest poprawna.`)
+            files.forEach(file => {
+                array_of_buffers.push(fs.readFileSync(`${folder}\\${file}`))
+              });
+              read_files()
+        })
+    }else{
+        alert('Wybierz folder')
+    }
+})
